6459b8e5eebb82e674092b681bfd504b198ec094
PriyanshuSharma0326 <ps940016@gmail.com>
2023-07-29 17:22:44 +0530

    Add input handler

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import InputForm from "./components/InputForm";
 
 export default function App() {
     return (
         <>
-            App
+            <InputForm />
         </>
     );
 }
---
+++ src/components/InputForm.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from 'react';
+import '../styles/InputForm.css';
+
+function InputForm() {
+    const [passwordLength, setPasswordLength] = useState(8);
+    const [password, setPassword] = useState('');
+
+    const [formCheckboxes, setFormInputs] = useState({
+        includeUppercase: true,
+        includeLowercase: true,
+        includeNumbers: true,
+        includeSymbols: true
+    })
+
+    const handlePasswordLengthChange = (e) => {
+        const length = parseInt(e.target.value);
+        setPasswordLength(length);
+    };
+
+    const inputHandler = (e) => {
+        setFormInputs({
+            ...formCheckboxes, 
+            [e.target.name]: e.target.checked
+        });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(formCheckboxes);
+
+        const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+        const numberChars = '0123456789';
+        const symbolChars = '!@#$%^&*()_-+=[]{}|:;"<>,.?/';
+      
+        let validChars = '';
+        let generatedPassword = '';
+      
+        if (formCheckboxes.includeUppercase) validChars += uppercaseChars;
+        if (formCheckboxes.includeLowercase) validChars += lowercaseChars;
+        if (formCheckboxes.includeNumbers) validChars += numberChars;
+        if (formCheckboxes.includeSymbols) validChars += symbolChars;
+      
+        const validCharsLength = validChars.length;
+      
+        for (let i = 0; i < passwordLength; i++) {
+            const randomIndex = Math.floor(Math.random() * validCharsLength);
+            generatedPassword += validChars.charAt(randomIndex);
+        }
+
+        setPassword(generatedPassword);
+    }
+
+    return (
+        <div className='form-container'>
+            <h1>Password Generator</h1>
+
+            <div className="password-container">
+                <p>{password}</p>
+            </div>
+
+            <form action="" className="password-form" onSubmit={handleSubmit}>
+                <div className="length-input">
+                    <label htmlFor="password-length">Password Length</label>
+
+                    <input 
+                        type="number" 
+                        value={passwordLength} 
+                        onChange={handlePasswordLengthChange}
+                        name="passwordLength" 
+                        id="password-length" 
+                        min='6' 
+                        max='20' 
+                    />
+                </div>
+
+                    <div className="conditional-input">
+                        <label htmlFor="uppercase">Include uppercase letters</label>
+                        <input 
+                            type="checkbox" 
+                            name="includeUppercase" 
+                            id="uppercase" 
+                            checked={formCheckboxes.includeUppercase}
+                            onChange={inputHandler}
+                        />
+                    </div>
+                    
+                    <div className="conditional-input">
+                        <label htmlFor="lowercase">Include lowercase letters</label>
+                        <input 
+                            type="checkbox" 
+                            name="includeLowercase" 
+                            id="lowercase" 
+                            checked={formCheckboxes.includeLowercase}
+                            onChange={inputHandler}
+                        />
+                    </div>
+
+                    <div className="conditional-input">
+                        <label htmlFor="numeric">Include numbers</label>
+                        <input 
+                            type="checkbox" 
+                            name="includeNumbers" 
+                            id="numeric" 
+                            checked={formCheckboxes.includeNumbers}
+                            onChange={inputHandler}
+                        />
+                    </div>
+
+                    <div className="conditional-input">
+                        <label htmlFor="symbolic">Include symbols</label>
+                        <input 
+                            type="checkbox" 
+                            name="includeSymbols" 
+                            id="symbolic" 
+                            checked={formCheckboxes.includeSymbols}
+                            onChange={inputHandler}
+                        />
+                    </div>
+
+                    <button className="generate-password-button">
+                        Generate Password
+                    </button>
+            </form>
+        </div>
+    );
+}
+
+export default InputForm;
